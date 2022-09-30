7033e337cfc7c853e36d2e7555d964743c9cf6ed
GoViper <nithishbanda2021@gmail.com>
2022-10-01 00:16:23 +0530

    inv

--- schemas/item-schema.js
+++ schemas/item-schema.js
@@ -7,7 +7,22 @@ const itemSchema = mongoose.Schema({
     userId: {
         type: String,
     },
-    name: {
+    userAvatar: {
+        type: String
+    },
+    orderId: {
+        type: String,
+    },
+    id : {
+        type: Number,
+    },
+    date: {
+        type: String,
+    },
+    userName: {
+        type: String,
+    },
+    itemName: {
         type: String,
     },
     url: {
@@ -15,7 +30,45 @@ const itemSchema = mongoose.Schema({
     },
     category : {
         type: String,
-    } 
+    },
+    cost : {
+        type: Number,
+    },
+    description : {
+        type: String,
+    },
+    premium : {
+        type: Boolean,
+        default: false
+    },
+    order: {
+        type: Boolean,
+        default: false
+    },
+    wallet: {
+        type: String
+    },
+    name: {
+        type: String
+    },
+    gender: {
+        type: String
+    },
+    country: {
+        type: String
+    },
+    zipCode: {
+        type: String
+    },
+    houseNumber: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    streetName: {
+        type: String
+    },
 })
 
 module.exports = mongoose.model('items', itemSchema)
